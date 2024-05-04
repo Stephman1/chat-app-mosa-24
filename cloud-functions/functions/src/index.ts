e69db70474e53087278ffda8533bebaafe98b18f
Ryan Morgan <RyanOnGithub@gmail.com>
2024-05-04 13:25:58 -0700

    Create Firebase Cloud Function that will store user metadata to firestore database

--- cloud-functions/functions/src/index.ts
+++ cloud-functions/functions/src/index.ts
@@ -1,19 +1,32 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
-
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
+/* example cloud function given by boilerplate */
 // export const helloWorld = onRequest((request, response) => {
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+import * as functions from "firebase-functions";
+import * as logger from "firebase-functions/logger";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
+
+initializeApp();
+
+const firestore = new Firestore();
+
+export const storeNewUserInFirebase = functions.auth.user().onCreate((user) => {
+  // extract user info
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+    displayName: user.displayName,
+  };
+
+  // where collection or doc does not exist, it will be created
+  firestore.collection("users").doc(user.displayName || user.uid).set(userInfo);
+
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+});
