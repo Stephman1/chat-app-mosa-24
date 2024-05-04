d10078cb754ebca58458d6c8674afc288a6b803c
Ryan Morgan <RyanOnGithub@gmail.com>
2024-05-03 22:08:05 -0700

    Create outline for Navbar component

---
+++ web-client/src/app/navbar/navbar.tsx
@@ -0,0 +1,14 @@
+'use client';
+
+import styles from "./navbar.module.css";
+
+function Navbar() {
+  return (
+    <nav className={styles.nav}>
+      <h1>DevChat</h1>
+      
+    </nav>
+  );
+}
+
+export default Navbar;
