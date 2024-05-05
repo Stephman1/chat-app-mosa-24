c995ecdb8b4661c76fa20bd77b99c9c585291df2
Ryan Morgan <RyanOnGithub@gmail.com>
2024-05-04 22:21:23 -0700

    Add functionality to enable / disable send button on chat page depending on authentication

--- web-client/src/app/chat/page.tsx
+++ web-client/src/app/chat/page.tsx
@@ -2,7 +2,9 @@
 
 import { eventNames } from "process";
 import styles from "./page.module.css";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { User, getAuth, onAuthStateChanged } from "firebase/auth";
+import { onAuthStateChangedHelper } from "@/utilities/firebase/firebase";
 
 export default function Chat() {
   const [inputText, setInputText] = useState("");
@@ -11,13 +13,19 @@ export default function Chat() {
     event.preventDefault();
     
     setInputText('');
-
-
-
     console.log(`message sent: ${inputText}`);
   };
 
-
+  /* Logic for enabling and disabling the send button */
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedHelper((user) => {
+      setUser(user);
+    });
+    // Cleanup subscription on unmount
+    return () => unsubscribe();
+  });
+  
   return (
     <main className={styles.main}>
       <div className={styles.chatbox}>
@@ -29,43 +37,49 @@ export default function Chat() {
             maxLength={160} placeholder="Type here..." onChange={(event) => {
               setInputText(event.target.value)
             }} />
-        <button className={styles.submitBtn} type="submit">Send</button>
+        {
+          (user === null) ? (
+            <button className={styles.submitBtnDisabled} type="submit" disabled>Send</button>
+          ) : (
+            <button className={styles.submitBtn} type="submit">Send</button>
+          )
+        }
       </form>
     </main>
   );
 }
         
-<!-- import { useEffect } from 'react';
-import styles from './page.module.css';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
-import { initializeApp } from "firebase/app";
+// import { useEffect } from 'react';
+// import styles from './page.module.css';
+// import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
+// import { initializeApp } from "firebase/app";
 
-export default function Chat() {
-  useEffect(() => {
-    const setMessages = () => {
-      async function printMessageData() {
-        // From Authentication Service creationYour web app's Firebase configuration
-        const firebaseConfig = {
-          apiKey: "AIzaSyAMC0z-vL9UQpH_-fRRB1VpjictRfM9800",
-          authDomain: "devchat-88614.firebaseapp.com",
-          projectId: "devchat-88614",
-          storageBucket: "devchat-88614.appspot.com",
-          messagingSenderId: "217891503392",
-          appId: "1:217891503392:web:5a2c296f8bbd602e186b45"
-        };
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        // Initialize Cloud Firestore and get a reference to the service
-        const db = getFirestore(app);
-        localStorage.clear();
-        // Fetch message data
-        const querySnapshot = await getDocs(collection(db, 'messages'));
-        querySnapshot.forEach((doc) => {
-          localStorage.setItem(doc.id.toString(), JSON.stringify(doc.data()));
-        });
-      }
-      printMessageData();
-    };
-    // Call setMessages function when component mounts
-    setMessages();
-  }, []); // Empty dependency array ensures this effect runs only once on component mount -->
+// export default function Chat() {
+//   useEffect(() => {
+//     const setMessages = () => {
+//       async function printMessageData() {
+//         // From Authentication Service creationYour web app's Firebase configuration
+//         const firebaseConfig = {
+//           apiKey: "AIzaSyAMC0z-vL9UQpH_-fRRB1VpjictRfM9800",
+//           authDomain: "devchat-88614.firebaseapp.com",
+//           projectId: "devchat-88614",
+//           storageBucket: "devchat-88614.appspot.com",
+//           messagingSenderId: "217891503392",
+//           appId: "1:217891503392:web:5a2c296f8bbd602e186b45"
+//         };
+//         // Initialize Firebase
+//         const app = initializeApp(firebaseConfig);
+//         // Initialize Cloud Firestore and get a reference to the service
+//         const db = getFirestore(app);
+//         localStorage.clear();
+//         // Fetch message data
+//         const querySnapshot = await getDocs(collection(db, 'messages'));
+//         querySnapshot.forEach((doc) => {
+//           localStorage.setItem(doc.id.toString(), JSON.stringify(doc.data()));
+//         });
+//       }
+//       printMessageData();
+//     };
+//     // Call setMessages function when component mounts
+//     setMessages();
+//   }, []); // Empty dependency array ensures this effect runs only once on component mount -->
